acaebb79abd8f7565d06e02ff5aac2b3da95597a
Jenniferplusplus <contact@jenniferplusplus.com>
2023-07-09 22:47:58 -0500

    handling for CompactIris

---
+++ Letterbook.ActivityPub.Tests/CompactIriTest.cs
@@ -0,0 +1,48 @@
+﻿namespace Letterbook.ActivityPub.Tests;
+
+public class CompactIriTest
+{
+    [Theory]
+    [InlineData("as:activity", "https://www.w3.org/ns/activitystreams#activity")]
+    [InlineData("xsd:doc", "http://www.w3.org/2001/XMLSchema#doc")]
+    [InlineData("schema:person", "http://schema.org/person")]
+    [InlineData("ldp:jobtitle", "http://www.w3.org/ns/ldp#jobtitle")]
+    public void ParseCompactStrings(string compact, string expected)
+    {
+        var success = CompactIri.TryCreateCompact(compact, out var actual);
+        
+        Assert.True(success);
+        Assert.Equal(new Uri(expected).ToString(), actual!.ToString());
+    }
+    
+    [Theory]
+    [InlineData("https://www.w3.org/ns/activitystreams#activity")]
+    [InlineData("http://www.w3.org/2001/XMLSchema#doc")]
+    [InlineData("ftp://example.com/source")]
+    [InlineData("user:password@example.com:8080/path?q=a#a:1")]
+    [InlineData("as:no:no")]
+    [InlineData("as:")]
+    [InlineData("invalid-prefix:no")]
+    [InlineData("http://192.168.1.1:8080/")]
+    public void FallbackToUri(string uri)
+    {
+        var created = CompactIri.TryCreateCompact(uri, out var actual);
+        
+        Assert.True(created);
+        Assert.Null(actual!.Namespace);
+        Assert.Equal(uri, actual.ToString());
+    }
+    
+    [Theory]
+    [InlineData("as:activity")]
+    [InlineData("xsd:doc")]
+    [InlineData("schema:person")]
+    [InlineData("ldp:jobtitle")]
+    public void FormatCompactUri(string compact)
+    {
+        var success = CompactIri.TryCreateCompact(compact, out var actual);
+        
+        Assert.True(success);
+        Assert.Equal(compact, actual!.ToCompact());
+    }
+}
---
+++ Letterbook.ActivityPub/CompactIri.cs
@@ -0,0 +1,85 @@
+﻿namespace Letterbook.ActivityPub;
+
+public class CompactIri : Uri
+{
+    public static Dictionary<string, string> Namespaces = new(new List<KeyValuePair<string, string>>(new[]
+    {
+        new KeyValuePair<string, string>("dc11", "http://purl.org/dc/elements/1.1/"),
+        new KeyValuePair<string, string>("dcterms", "http://purl.org/dc/terms/"),
+        new KeyValuePair<string, string>("cred", "https://w3id.org/credentials#"),
+        new KeyValuePair<string, string>("foaf", "http://xmlns.com/foaf/0.1/"),
+        new KeyValuePair<string, string>("geojson", "https://purl.org/geojson/vocab#"),
+        new KeyValuePair<string, string>("prov", "http://www.w3.org/ns/prov#"),
+        new KeyValuePair<string, string>("i18n", "https://www.w3.org/ns/i18n#"),
+        new KeyValuePair<string, string>("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+        new KeyValuePair<string, string>("schema", "http://schema.org/"),
+        new KeyValuePair<string, string>("skos", "http://www.w3.org/2004/02/skos/core#"),
+        new KeyValuePair<string, string>("xsd", "http://www.w3.org/2001/XMLSchema#"),
+        new KeyValuePair<string, string>("as", "https://www.w3.org/ns/activitystreams#"),
+        new KeyValuePair<string, string>("ldp", "http://www.w3.org/ns/ldp#"),
+        new KeyValuePair<string, string>("vcard", "http://www.w3.org/2006/vcard/ns#"),
+    }));
+
+    public string? Namespace { get; init; }
+    public string? Prefix { get; init; }
+    public string? Suffix { get; init; }
+
+    public CompactIri(string ns, string prefix, string suffix) : base(ns + suffix)
+    {
+        Namespace = ns;
+        Prefix = prefix;
+        Suffix = suffix;
+    }
+
+    public CompactIri(string prefix, string suffix) : this(Namespaces[prefix], prefix, suffix)
+    {
+    }
+
+    public CompactIri(string uri) : base(uri)
+    {
+    }
+    
+    public CompactIri(Uri uri) : base(uri.ToString())
+    {}
+
+    public static bool TryCreateCompact(string compactUrl, out CompactIri? value)
+    {
+        if (IsCompactUri(compactUrl, out var prefix, out var suffix)
+            && Namespaces.ContainsKey(prefix!)
+            && prefix != string.Empty
+            && suffix != string.Empty)
+        {
+            value = new CompactIri(prefix!, suffix!);
+            return true;
+        }
+
+        var result = TryCreate(compactUrl, UriKind.Absolute, out var uri);
+        value = result ? new CompactIri(uri!) : default;
+        return result;
+    }
+
+    public static bool IsCompactUri(string compactUrl, out string? prefix, out string? suffix)
+    {
+        prefix = default;
+        suffix = default;
+        // unknown indicates it's likely not a valid hostname
+        // false on scheme name indicates it doesn't have a valid scheme
+        // in either case, this is likely a compacturi, rather than absolute
+        if (CheckHostName(compactUrl) != UriHostNameType.Unknown && CheckSchemeName(compactUrl))
+        {
+            return false;
+        }
+
+        var parts = compactUrl.Split(":");
+        if (parts.Length != 2)
+        {
+            return false;
+        }
+
+        prefix = parts[0];
+        suffix = parts[1];
+        return true;
+    }
+
+    public string ToCompact() => Prefix != null ? string.Join(":", Prefix, Suffix) : ToString();
+}
